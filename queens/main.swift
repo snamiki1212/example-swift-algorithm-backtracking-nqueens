750d8eff9daa1d01ee55c72196a961cf1c90c2f9
snamiki1212 <snamiki1212@gmail.com>
2021-07-30 15:45:46 -0700

    Refactor main logic

--- queens/main.swift
+++ queens/main.swift
@@ -8,12 +8,17 @@
 import Foundation
 
 // ----------------
-var board = Board.init(size: 8)
+var board: Board;
+
+// All case
 print("=======")
+board = Board.init(size: 8)
 solve8Queens(board: &board, type: Type.all)
 print("total call num: ", total, "numOfCase: ", numOfCase)
+
+// First case
 print("=======")
 board = Board.init(size: 8)
 solve8Queens(board: &board, type: Type.first)
 print("total call num: ", total, "numOfCase: ", numOfCase)
-print("=======")
+
