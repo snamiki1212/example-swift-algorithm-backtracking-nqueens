bf28b7ae084dd984ab55da50462ec44283be9abc
snamiki1212 <snamiki1212@gmail.com>
2021-07-30 15:58:21 -0700

    Update right

--- queens/QueensSolver.swift
+++ queens/QueensSolver.swift
@@ -1,19 +1,10 @@
 //
 //  QueensSolver.swift
-//  SwiftAGDS
 //
-//  Created by Derrick Park on 2019-03-13.
-//  Copyright © 2019 Derrick Park. All rights reserved.
+//  Created by shunnamiki on 2021/07/30.
 //
 
 import Foundation
-
-/// Write a function solveQueens that accepts a Board as a parameter
-/// and tries to place 8 queens on it safely.
-///
-/// - Your method should stop exploring if it finds a solution
-/// - You are allowed to change the function header (args or return type)
-/// - Your total recursive calls should not exceed 120 times.
 
 var total = 0;
 var numOfCase = 0;
